3a6c22503339efbe8e6e13d72e75ad03bf67039b
zief0002 <zief0002@umn.edu>
2023-06-06 10:29:29 -0500

    Updated t_results()

--- R/t_results.R
+++ R/t_results.R
@@ -13,13 +13,27 @@ t_results = function(x){
 
   inequality_sign = switch(x$alternative, "less"="<", "greater"=">", "two.sided"="≠")
 
-  cat(
-    "\n", rep("-", 50), "\n", x$method, "\n", rep("-", 50), "\n",
-    "\nH[0]: mu = ", x$null.value,
-    "\nH[A]: mu ", inequality_sign, " ", x$null.value,
-    "\n\nt(", x$parameter, ") = ", x$statistic,
-    "\np = ", x$p.value,
-    "\n\n", rep("-", 50), "\n",
-    sep = "")
+  if (x$method == "One Sample t-test"){
+    cat(
+      "\n", rep("-", 50), "\n", x$method, "\n", rep("-", 50), "\n",
+      "\nH[0]: mu = ", x$null.value,
+      "\nH[A]: mu ", inequality_sign, " ", x$null.value,
+      "\n\nt(", x$parameter, ") = ", x$statistic,
+      "\np = ", x$p.value,
+      "\n\n", rep("-", 50), "\n",
+      sep = "")
+  } else{
+    # Extract group names
+    group1 = gsub(x = names(x$estimate)[1], pattern = "mean in group ", replacement = "")
+    group2 = gsub(x = names(x$estimate)[2], pattern = "mean in group ", replacement = "")
 
+    cat(
+      "\n", rep("-", 50), "\n", x$method, "\n", rep("-", 50), "\n",
+      "\nH[0]: mu_[", group1, "] = mu_[", group2, "]",
+      "\nH[A]: mu_[", group1, "] ", inequality_sign, " mu_[", group2, "]",
+      "\n\nt(", x$parameter, ") = ", x$statistic,
+      "\np = ", x$p.value,
+      "\n\n", rep("-", 50), "\n",
+      sep = "")
+    }
 }
