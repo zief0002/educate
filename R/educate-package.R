d183982d18baadfc535e96134a9f37a46f571c78
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2026-02-19 13:44:36 -0600

    Pkg level imports, usethis import_from byproduct

---
+++ R/educate-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom mosaic prop_test
+#' @importFrom patchwork wrap_plots
+#' @importFrom stats dnorm
+#' @importFrom stats dt
+#' @importFrom stats qnorm
+## usethis namespace: end
+NULL
